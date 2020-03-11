730a37d47bf4e55b3fbfe82e7d56a5d35fd03e68
Leonid Vlasov <leoniv.vlasov@gmail.com>
2020-03-11 19:23:39 +0700

    init test

---
+++ src/test/scala/hello_cats/EldisTestSpec.scala
@@ -0,0 +1,12 @@
+package hello_cats
+
+import org.scalatest._
+import java.util.regex.Matcher
+
+class EldisTestSpec extends FunSpec with Matchers {
+  describe("#readClosure") {
+    it ("FIXME") {
+      true should be(false)
+    }
+  }
+}
