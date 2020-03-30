26254907a5fc625b7f5b85f8fd67c88d493311d9
Leonid Vlasov <leoniv.vlasov@gmail.com>
2020-03-30 15:09:14 +0700

    Progress ValidatorLib

--- src/main/scala/hello_cats/ValidatorLib.scala
+++ src/main/scala/hello_cats/ValidatorLib.scala
@@ -6,19 +6,24 @@ import cats.data.NonEmptyList
 import cats.syntax.validated._
 import cats.syntax.apply._
 import cats.kernel.Semigroup
+import cats.data.Validated._
+import Function.const
 
 object ValidatorLib {
-  type Error = NonEmptyList[String]
-  type ValidatedL[A] = Validated[Error, A]
-  type Input[A, B] = ReaderT[ValidatedL, A, B]
+  type Errors = NonEmptyList[String]
+  type Result[A] = Either[Errors, A]
+  type Check[A, B] = ReaderT[Result, A, B]
+  type Input[A] = Map[String, A]
 
   sealed trait Predicate[E, A] {
     import Predicate._
 
     def apply(a: A)(implicit s: Semigroup[E]): Validated[E, A]
-    def run(a: A)(implicit s: Semigroup[E]): Either[E, A] = this(a).toEither
+    def run(a: A)(implicit s: Semigroup[E]): Validated[E, A] = this(a)
     def or(other: Predicate[E,A]) = Or(this, other)
     def and(other: Predicate[E,A]) = And(this, other)
+    def contramap[B](f: B => A)(implicit s: Semigroup[E]): Predicate[E, B] =
+      Pure((b: B) => apply(f(b)) map (const(b)))
   }
 
   object Predicate {
@@ -29,7 +34,13 @@ object ValidatorLib {
     case class Or[E, A](
       left: Predicate[E, A],
       right: Predicate[E, A]) extends Predicate[E, A] {
-      def apply(a: A)(implicit s: Semigroup[E]) = ???
+      def apply(a: A)(implicit s: Semigroup[E]) = left(a) match {
+        case Valid(_) => Valid(a)
+        case Invalid(e1) => right(a) match {
+          case Valid(_) => Valid(a)
+          case Invalid(e2) => Invalid(s.combine(e1, e2))
+        }
+      }
     }
 
     case class And[E, A](
@@ -45,7 +56,52 @@ object ValidatorLib {
         )
   }
 
+  def check[A, B](fn: A => Result[B]): Check[A, B] = ReaderT(fn)
+
+  def pred2check[A](p: Predicate[Errors, A]): Check[A, A] = check((a: A) =>
+      p(a).toEither
+    )
+
+//FIXME  def checkCombine[A, B](p: Predicate[Errors, A]): Check[A, B]
+
+  def check2pred[A](ch: Check[A, _]): Predicate[Errors, A] =
+    Predicate((a: A) => Validated.fromEither(ch.run(a)) map const(a))
+
   def error(m: String) = NonEmptyList(m, Nil)
 
-  def longerThan(n: Integer) = ???
+  private def keyNotFoundError(k: String) = error(s"Key `$k' not found")
+
+  def readKey[A](k: String): Check[Input[A], A] = check( (m: Input[A]) =>
+    m.get(k).toRight(keyNotFoundError(k))
+  )
+
+  def mustBeKey(k: String): Predicate[Errors, Input[_]] =
+    check2pred(readKey(k))
+
+  def longerThan(n: Int): Predicate[Errors, String] =
+    Predicate.lift(
+      error(s"Must be longer than $n characters"),
+      str => str.size > n)
+
+  val alphanumeric: Predicate[Errors, String] =
+    Predicate.lift(
+      error(s"Must be all alphanumeric characters"),
+      str => str.forall(_.isLetterOrDigit))
+
+  def isInt: Predicate[Errors, String] =
+    Predicate.lift(
+      error("Must be a number"),
+      str => """^-?\d+""".r.matches(str)
+    )
+
+
+  def contains(char: Char): Predicate[Errors, String] =
+    Predicate.lift(
+      error(s"Must contain the character $char"),
+      str => str.contains(char))
+
+  def containsOnce(char: Char): Predicate[Errors, String] =
+    Predicate.lift(
+      error(s"Must contain the character $char only once"),
+      str => str.filter(c => c == char).size == 1)
 }
--- src/test/scala/hello_cats/ValidatorLibSpec.scala
+++ src/test/scala/hello_cats/ValidatorLibSpec.scala
@@ -5,47 +5,155 @@ import cats.data.Validated._
 import hello_cats.ValidatorLib._
 import org.scalatest.freespec.AnyFreeSpec
 import cats.instances.list._
+import cats.instances.either._
 
 class ValidatorLibSpec extends AnyFreeSpec with Matchers {
-  case class Input(name: String, age: String, email: String)
   case class User(name: String, age: Int, email: String)
 
   "Predicate" - {
     val error = (m: String) => List(m)
     val invalid = (m: String) => Predicate.lift(error(m), (_: Any) => false)
     val valid = (_: String) => Predicate.lift(error(""), (_: Any) => true)
+
     "Or" in {
-       assert((invalid("1") or invalid("2") apply "") == Invalid(List("1","2")))
-       assert((valid("") or valid("") apply "value") == Valid("value"))
-       assert((valid("") or invalid("2") apply "value") == Valid("value"))
-       assert((invalid("1") or valid("") apply "value") == Valid("value"))
+      assert((invalid("1") or invalid("2") apply "") == Invalid(List("1", "2")))
+      assert((valid("") or valid("") apply "value") == Valid("value"))
+      assert((valid("") or invalid("2") apply "value") == Valid("value"))
+      assert((invalid("1") or valid("") apply "value") == Valid("value"))
     }
 
     "And" in {
-      assert((invalid("1") and invalid("2") apply "") == Invalid(List("1","2")))
+      assert(
+        (invalid("1") and invalid("2") apply "") == Invalid(List("1", "2"))
+      )
       assert((invalid("1") and valid("") apply "") == Invalid(List("1")))
       assert((valid("") and invalid("2") apply "") == Invalid(List("2")))
       assert((valid("") and valid("2") apply "value") == Valid("value"))
     }
   }
 
-
-  "#makeUser" - {
-    def makeUser(in: Input): ValidatedL[User] = ???
-
-    "When input contains valid data should make instance of User" in {
-      makeUser(Input("Name", "42", "name@example.com")) should
-        be (Valid(User("Name", 42, "name@example.com")))
+  "Helpers" - {
+    "#readKey" in {
+      readKey("k").run(Map()) should be(Left(error("Key `k' not found")))
+      readKey("k").run(Map("k" -> 1)) should be(Right(1))
     }
 
-    "When input contains invalid data sould returns errors" in {
-      makeUser(Input("", "-1", "bad name@badhost")) should
-        be (Invalid(List("Must be none empty string",
-                      "Must be number more than 16",
-                      "Must be one word",
-                      "Must have dot")
-                    )
-                  )
+    "#mustBeKey" in {
+      mustBeKey("k").run(Map()) should be(Invalid(error("Key `k' not found")))
+      mustBeKey("k").run(Map("k" -> 1)) should be(Valid(Map("k" -> 1)))
     }
+
+    "#isInt" in {
+      isInt("-123") should be(Valid("-123"))
+      isInt("123") should be(Valid("123"))
+      isInt("123") should be(Valid("123"))
+      isInt("abc") should be(Invalid(error("Must be a number")))
+    }
+  }
+
+  def const[A, B](a: A)(b: B): A = a
+
+  "Example #makeUser" - {
+    val validInput = Map(
+      "name" -> "Name",
+      "age" -> "42",
+      "email" -> "name@example.com"
+    )
+
+    val invalidInput = Map(
+      "name" -> "",
+      "age" -> "NaN",
+      "email" -> "example.com"
+    )
+
+    def readAge: Check[Input[String], Int] = for {
+      age <- readKey[String]("age")
+      _ <- pred2check(isInt.contramap(const[String, Input[String]](age)))
+    } yield age.toInt
+
+    def validateAge: Predicate[Errors, Input[_]] = ??? /*
+    check2pred(
+      readKey[String]("age") flatMap { age =>
+        check(in => isInt.contramap(in => age))
+      }
+
+    // flatMap ((age: String) => pred2check(isInt(age)))
+    )
+    */
+
+    def readInputMonadic =
+      for {
+        name <- readKey[String]("name")
+        age <- readKey[String]("age")
+        email <- readKey[String]("email")
+      } yield (name, age, email)
+
+    def readInputApplicative = (
+      mustBeKey("name") and
+      validateAge and
+      mustBeKey("email")
+    )
+
+    "Fail-fast input read" - {
+      "Read succsess" in {
+        readInputMonadic.run(validInput) should
+          be(Right(("Name", "42", "name@example.com")))
+      }
+
+      "Read failure" in {
+        readInputMonadic.run(validInput - "age") should
+          be(Left(error("Key `age' not found")))
+      }
+    }
+
+    "Multy error input read" - {
+      "Read success" in {
+        readInputApplicative(validInput) should
+          be(Valid(validInput))
+      }
+
+      "Read error" - {
+        "When key not found" in {
+          readInputApplicative { validInput - "name" - "email" } should
+            be(
+              Invalid(
+                "Key `name' not found" ::
+                  error("Key `email' not found")
+              )
+            )
+        }
+
+        "When value invalid" in {
+          readInputApplicative { invalidInput - "name" - "email" } should
+            be(
+              Invalid(
+                "Key `name' not found" ::
+                  "Must be a number" ::
+                  error("Key `email' not found")
+              )
+            )
+        }
+      }
+    }
+
+//    def makeUser(in: Input[String]) = {
+//      (checkName, checkAge, checkEmail)
+//    }
+//
+//
+//    "When input contains valid data should make instance of User" in {
+//      makeUser(validInput) should
+//        be (Valid(User("Name", 42, "name@example.com")))
+//    }
+//
+//    "When input contains invalid data sould returns errors" in {
+//      makeUser(invalidInput) should
+//        be (Invalid(List("Must be none empty string",
+//                      "Must be a number",
+//                      "Must be one word",
+//                      "Must have dot")
+//                    )
+//                  )
+//    }
   }
 }
