eb0dba7983d59eb1b1d00b264e4848c534880dce
Leonid Vlasov <leoniv@eldis.ru>
2020-04-25 11:29:23 +0700

    Functor complite

--- fp-in-scala/src/main/scala/Functor.scala
+++ fp-in-scala/src/main/scala/Functor.scala
@@ -11,7 +11,10 @@ object Functor {
   object instances {
     implicit def eitherFunctor[A]: Functor[Either[A, *]] =
       new Functor[Either[A, *]] {
-        def fmap[B, C](f: Either[A, B])(g: B => C): Either[A, C] = f.map(g)
+        def fmap[B, C](f: Either[A, B])(g: B => C): Either[A, C] = f match {
+          case Right(a) => Right(g(a))
+          case Left(b) => Left(b)
+        }
       }
 
     implicit def optionFunctor: Functor[Option[*]] =
@@ -23,7 +26,7 @@ object Functor {
   object ops {
     implicit def functorOps[F[_], A](
         target: F[A]
-    )(implicit tc: Functor[F]): Ops[F,A] = new Ops[F,A] {
+    )(implicit tc: Functor[F]): Ops[F, A] = new Ops[F, A] {
 
       def typeClassInstance = tc
       def self = target
@@ -34,7 +37,5 @@ object Functor {
     def typeClassInstance: Functor[F]
     def self: F[A]
     def fmap[B](f: A => B): F[B] = typeClassInstance.fmap(self)(f)
-    def liftF[B](f: A => B): F[A] => F[B] = typeClassInstance.liftF(f)
-    def <%> = liftF _
   }
 }
--- fp-in-scala/src/test/scala/FunctorSpec.scala
+++ fp-in-scala/src/test/scala/FunctorSpec.scala
@@ -6,18 +6,27 @@ import Functor.ops._
 
 class FunctorSpec extends Specification {
   type EitherN[A] = Either[Nothing, A]
+  type EitherI[A] = Either[Int, A]
 
   implicit class IntToEither(i: Int) {
     def left[A]: Either[Int, A] = Left(i)
     def right[A]: Either[A, Int] = Right(i)
   }
 
+  implicit class IntToOption(i: Int) {
+    def some: Option[Int] = Some(i)
+    def none: Option[Int] = None
+  }
+
   "With object Functor examples".p
   eg { Functor[EitherN].fmap(1.right) { _ + 1 } must beRight(2) }
+  eg { Functor[EitherI].fmap(1.left[Int]) { _ + 1 } must beLeft(1) }
   eg { Functor[EitherN].liftF((_: Int) + 1)(1.right) must beRight(2) }
 
   "With syntax Functor exmples".p
-  eg { 1.right fmap { _ + 1 } must beRight(2) }
-  //eg { Some(1) <%> ((_: Int) + 1) must beSome(2) }
+  eg { 1.right[Nothing] fmap { _ + 1 } must beRight(2) }
+  eg { 1.left[Int] fmap { _ + 1 } must beLeft(1) }
+  eg { 1.some fmap ((_: Int) + 1) must beSome(2) }
+  eg { 1.none fmap ((_: Int) + 1) must beNone }
 
 }
