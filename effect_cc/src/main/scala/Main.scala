2cdae8d484de99af5047a43963e70caaa729d126
Leonid Vlasov <leoniv@eldis.ru>
2020-05-05 09:00:17 +0700

    Generalize effect_cc/Main

--- effect_cc/src/main/scala/Main.scala
+++ effect_cc/src/main/scala/Main.scala
@@ -4,41 +4,41 @@ import java.io._
 
 object Main extends IOApp {
 
-  def srcFile(file: File): Resource[IO, FileInputStream] =
-    Resource.fromAutoCloseable(IO.delay(new FileInputStream(file)))
+  def srcFile[F[_]: Sync](file: File): Resource[F, FileInputStream] =
+    Resource.fromAutoCloseable(Sync[F].delay(new FileInputStream(file)))
 
-  def destFile(file: File): Resource[IO, FileOutputStream] =
-    Resource.fromAutoCloseable(IO.delay(new FileOutputStream(file)))
+  def destFile[F[_]: Sync](file: File): Resource[F, FileOutputStream] =
+    Resource.fromAutoCloseable(Sync[F].delay(new FileOutputStream(file)))
 
-  def transfer(src: InputStream, dest: OutputStream): IO[Long] = {
+  def transfer[F[_]: Sync](src: InputStream, dest: OutputStream): F[Long] = {
     def go(
         src: InputStream,
         dest: OutputStream,
         buf: Array[Byte],
         total: Long
-    ): IO[Long] =
+    ): F[Long] =
       for {
-        count <- IO(src.read(buf))
-        total <- if (count < 1) IO(total) else go(src, dest, buf, total + count)
+        count <- Sync[F].delay(src.read(buf))
+        total <- if (count < 1) Sync[F].delay(total) else go(src, dest, buf, total + count)
       } yield total
     go(src, dest, new Array[Byte](1024), 0L)
   }
 
-  def copy(src: File, dest: File): IO[Long] =
+  def copy[F[_]: Sync](src: File, dest: File): F[Long] =
     (for {
       src <- srcFile(src)
       dest <- destFile(dest)
-    } yield (src -> dest)).use((transfer _).tupled)
+    } yield (src -> dest)).use((transfer[F] _).tupled)
 
-  def copy0(src: File, dest: File): Resource[IO, Long] =  for {
-    src <- srcFile(src)
-    dest <- destFile(dest)
+  def copy0[F[_]: Sync](src: File, dest: File): Resource[F, Long] =  for {
+    src <- srcFile[F](src)
+    dest <- destFile[F](dest)
     total <- Resource.liftF(transfer(src, dest))
   } yield total
 
   override def run(args: List[String]): IO[ExitCode] = args match {
     case (src :: dest :: tail) =>
-      copy0(new File(src), new File(dest))
+      copy0[IO](new File(src), new File(dest))
         .map(total => println(s"Copy $total")).use(_ => IO(ExitCode.Success))
     case _ => IO(println("Usage: copy src dest")) *> IO(ExitCode.Error)
   }
